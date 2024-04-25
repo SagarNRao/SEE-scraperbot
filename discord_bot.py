900f79b96ddf285173ba3b03c765c977bf584a25
Pushan T <pushan.sahanaganesh@gmail.com>
2024-04-25 13:47:35 +0530

    Added error logging for token limit during video summary

--- discord_bot.py
+++ discord_bot.py
@@ -16,14 +16,17 @@ bot = commands.Bot(command_prefix='!', intents=intents, help_command=None)
 
 @bot.command()
 async def search(ctx, *args):
-    arguments = ' '.join(args)
-    obj.get_videos(arguments)
-    for key in obj.video_dict:
-        link = ""
-        link = "https://youtube.com" + str(obj.video_dict[key])
-        await ctx.send(f'{key}: \n {link}')
-        time.sleep(2)
-    await ctx.send('Done!')
+    try:
+        arguments = ' '.join(args)
+        obj.get_videos(arguments)
+        for key in obj.video_dict:
+            link = ""
+            link = "https://youtube.com" + str(obj.video_dict[key])
+            await ctx.send(f'{key}: \n {link}')
+            time.sleep(2)
+        await ctx.send('Done!')
+    except Exception as e:
+        raise e
     
 @bot.command()
 async def summary(ctx, *args):
@@ -36,13 +39,22 @@ async def summary(ctx, *args):
             link = obj.video_dict[key]
             summary = obj.get_video_summary(link)
             await ctx.send(f'Here\'s a summay of the video {key}:\n{summary}')
-        except:
-            await ctx.send('Invalid Command (number likely missing)')
+        except Exception as e:
+            # await ctx.send('Invalid Command (number likely missing)')
+            await ctx.send(f'Error: {str(e)}')
 
 @bot.command()
 async def help(ctx):
-    options = '''```!search <search querey>```\nShows a list of search results that would appear if you searched the query on YouTube.\n```!summary <query>```\nIf you have already used the !search command before, you can replace the <query> with index-xyz to get the summary for a video from the search results.\nExample: `!summary index-2` will return the summary for the second video from the search results.'''
+    options = '''```!search <search querey>```\nShows a list of search results that would appear if you searched the query on YouTube.\n```!summary <query>```\nIf you have already used the !search command before, you can replace the <query> with index-xyz to get the summary for a video from the search results.\nExample: `!summary index-2` will return the summary for the second video from the search results.\nIMPORTANT NOTE: If the video is too long, the summary wont be generated due to AI token limitations'''
     await ctx.send(f'**Here is a list of commands:**\n{options}')
+    
+@bot.command()
+async def vidlist(ctx):
+    try:
+        vid_dict_str = '\n'.join(f'{k}: {v}' for k, v in obj.video_dict.items())
+        await ctx.send(f'Here is the video dictionary:\n{vid_dict_str}')
+    except Exception as e:
+        await ctx.send(f'Error: {str(e)}')
 
 bot_token = os.getenv('BOT_TEST_KEY')
 bot.run(bot_token)
